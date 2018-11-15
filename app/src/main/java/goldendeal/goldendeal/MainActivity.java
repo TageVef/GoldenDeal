cca9b2bcc0bbf2e15e90e52850350d920fed5ded
Tage Veflingstad <Tage.veflingstad@gmail.com>
2018-11-15 19:32:46 +0100

    added code to the add and remove poinst buttons
    added functionality to the add and remove points buttons. and makes the points displayed on the bank screen

--- app/src/main/java/goldendeal/goldendeal/MainActivity.java
+++ app/src/main/java/goldendeal/goldendeal/MainActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
     private Button addPointsButton;
     private Button removePointsButton;
     private ViewFlipper windowFlipper;
+    private TextView pointsView;
+    private TextView pointsTitleField;
+    private TextView rules;
+
+    private int points = 0;
+    private String pointsTitle = "Points";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,43 +46,78 @@ public class MainActivity extends AppCompatActivity {
         addPointsButton = (Button) findViewById(R.id.addPoints);
         removePointsButton = (Button) findViewById(R.id.removePoints);
         windowFlipper = (ViewFlipper) findViewById(R.id.myViewFlipper);
+        pointsView = (TextView) findViewById(R.id.Points);
+        pointsTitleField = (TextView) findViewById(R.id.pointTitle);
+        rules = (TextView) findViewById(R.id.Rules);
+
 
         View.OnClickListener switchPage = new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
+                pointsView.setText("");
+                pointsTitleField.setText("");
                 Button switchButton = (Button) view;
                 String buttonText = switchButton.getText().toString();
-                Log.d(TAG, "onClick: button pushed " + buttonText);
-
                 if(buttonText == taskButton.getText().toString())
                 {
-                    Log.d(TAG, "Tasks");
                     windowFlipper.setDisplayedChild(windowFlipper.indexOfChild(findViewById(R.id.taskView)));
                 }
                 else if(buttonText == shopButton.getText().toString())
                 {
-                    Log.d(TAG, "Shop");
                     windowFlipper.setDisplayedChild(windowFlipper.indexOfChild(findViewById(R.id.shopView)));
                 }
                 else if(buttonText == bankButton.getText().toString())
                 {
-                    Log.d(TAG, "Bank");
                     windowFlipper.setDisplayedChild(windowFlipper.indexOfChild(findViewById(R.id.bankView)));
+                    pointsView.setText(Integer.toString(points));
+                    pointsTitleField.setText(pointsTitle + ": ");
                 }
                 else if(buttonText == rulesButton.getText().toString())
                 {
-                    Log.d(TAG, "Rules");
+                    rules.setText("1. we most follow the rules! \n\n 2. all tasks must be done by the description to get accepted");
                     windowFlipper.setDisplayedChild(windowFlipper.indexOfChild(findViewById(R.id.rulesView)));
                 }
             }
         };
 
+        View.OnClickListener addRemovePoints = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String buttonText = ((Button) view).getText().toString();
+
+                if(buttonText == addPointsButton.getText().toString())
+                {
+                    points += 10;
+                }
+                else if(buttonText == removePointsButton.getText().toString())
+                {
+                    if(points >= 10)
+                    {
+                        points -= 10;
+                    }
+                    else if(points < 10 && points > 0)
+                    {
+                        Log.d(TAG, "cant buy something you cant afford!");
+                    }
+                    else
+                    {
+                        Log.d(TAG, "you dont even have any points to buy this!");
+                    }
+                }
+            }
+        };
+
+
         taskButton.setOnClickListener(switchPage);
         shopButton.setOnClickListener(switchPage);
         bankButton.setOnClickListener(switchPage);
         rulesButton.setOnClickListener(switchPage);
+        addPointsButton.setOnClickListener(addRemovePoints);
+        removePointsButton.setOnClickListener(addRemovePoints);
     }
 
     @Override
