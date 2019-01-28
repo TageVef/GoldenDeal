e935a734e840fe55b21e7026540495cae214fc74
Tage Veflingstad <Tage.veflingstad@gmail.com>
2019-01-28 15:22:43 +0100

    database
    setting up communication with the database for retriving information from the database

--- app/src/main/java/goldendeal/goldendeal/Activities/MainActivities/TasksActivity.java
+++ app/src/main/java/goldendeal/goldendeal/Activities/MainActivities/TasksActivity.java
@@ -1,21 +1,26 @@
 package goldendeal.goldendeal.Activities.MainActivities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.constraint.Guideline;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.ViewFlipper;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import goldendeal.goldendeal.Activities.OptionsActivity;
+import goldendeal.goldendeal.Model.User;
 import goldendeal.goldendeal.R;
 
 public class TasksActivity extends AppCompatActivity {
@@ -27,11 +32,15 @@ public class TasksActivity extends AppCompatActivity {
     private FirebaseDatabase mDatabase;
     private FirebaseUser mUser;
     private FirebaseAuth mAuth;
+    //------------------------------------------------------
 
     private ImageButton taskButton;
-    private ImageButton shopButton;
+    private ImageButton storeButton;
     private ImageButton bankButton;
     private ImageButton rulesButton;
+    private Button optionsButton;
+
+    private User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,46 +55,46 @@ public class TasksActivity extends AppCompatActivity {
         mDatabaseReference.keepSynced(true);
 
         taskButton = (ImageButton) findViewById(R.id.TaskButton);
-        shopButton = (ImageButton) findViewById(R.id.ShopButton);
+        storeButton = (ImageButton) findViewById(R.id.StoreButton);
         bankButton = (ImageButton) findViewById(R.id.BankButton);
         rulesButton = (ImageButton) findViewById(R.id.RulesButton);
+        optionsButton = (Button) findViewById(R.id.optionsButton);
 
-        mDatabaseReference.child("Users").child(mUser.toString());
 
-
-        View.OnClickListener switchPage = new View.OnClickListener()
-        {
+        View.OnClickListener switchPage = new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 ImageButton switchButton = (ImageButton) view;
-                int buttonText = switchButton.getId();
-                if(buttonText == taskButton.getId())
-                {
-                    startActivity(new Intent(TasksActivity.this, TasksActivity.class));
-                    finish();
-                }
-                else if(buttonText == shopButton.getId())
-                {
-                    startActivity(new Intent(TasksActivity.this, StoreActivity.class));
-                    finish();
-                }
-                else if(buttonText == bankButton.getId())
-                {
-                    startActivity(new Intent(TasksActivity.this, BankActivity.class));
-                    finish();
-                }
-                else if(buttonText == rulesButton.getId())
-                {
-                    startActivity(new Intent(TasksActivity.this, RulesActivity.class));
-                    finish();
+                int buttonText = view.getId();
+                switch (buttonText) {
+                    case R.id.TaskButton:
+                        startActivity(new Intent(TasksActivity.this, TasksActivity.class));
+                        finish();
+                        break;
+                    case R.id.StoreButton:
+                        startActivity(new Intent(TasksActivity.this, StoreActivity.class));
+                        finish();
+                        break;
+                    case R.id.BankButton:
+                        startActivity(new Intent(TasksActivity.this, BankActivity.class));
+                        finish();
+                        break;
+                    case R.id.RulesButton:
+                        startActivity(new Intent(TasksActivity.this, RulesActivity.class));
+                        finish();
+                        break;
+                    case R.id.optionsButton:
+                        startActivity(new Intent(TasksActivity.this, OptionsActivity.class));
+                        finish();
+                        break;
                 }
             }
         };
 
         taskButton.setOnClickListener(switchPage);
-        shopButton.setOnClickListener(switchPage);
+        storeButton.setOnClickListener(switchPage);
         bankButton.setOnClickListener(switchPage);
         rulesButton.setOnClickListener(switchPage);
+        optionsButton.setOnClickListener(switchPage);
     }
 }
