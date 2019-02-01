e3517a9ebb8cc7bf8a9bdd321914e42951d92124
Tage Veflingstad <Tage.veflingstad@gmail.com>
2019-02-01 12:33:31 +0100

    Task recycler view
    fixed the recycler view to show the current tasks for the user.

--- app/src/main/java/goldendeal/goldendeal/Activities/UserActivities/TasksActivity.java
+++ app/src/main/java/goldendeal/goldendeal/Activities/UserActivities/TasksActivity.java
@@ -2,8 +2,11 @@ package goldendeal.goldendeal.Activities.UserActivities;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,13 +14,19 @@ import android.widget.ImageButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import goldendeal.goldendeal.Activities.OptionsActivity;
+import goldendeal.goldendeal.Data.TaskRecyclerAdapter;
+import goldendeal.goldendeal.Model.Task;
 import goldendeal.goldendeal.Model.User;
 import goldendeal.goldendeal.R;
 
@@ -37,13 +46,19 @@ public class TasksActivity extends AppCompatActivity {
     private ImageButton bankButton;
     private ImageButton rulesButton;
     private Button optionsButton;
+    private RecyclerView recyclerView;
 
     private User currentUser;
+    private TaskRecyclerAdapter taskRecyclerAdapter;
+    private List<Task> taskList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+
+        InitiateButtons();
 
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
@@ -56,12 +71,57 @@ public class TasksActivity extends AppCompatActivity {
 
         mDatabaseReference = mDatabase.getReference();
 
+        taskList = new ArrayList<>();
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mDatabaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Task currentTask = dataSnapshot.getValue(Task.class);
+                taskList.add(currentTask);
+
+                taskRecyclerAdapter = new TaskRecyclerAdapter(TasksActivity.this, taskList);
+                recyclerView.setAdapter(taskRecyclerAdapter);
+                taskRecyclerAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void InitiateButtons() {
         taskButton = (ImageButton) findViewById(R.id.TaskButton);
         storeButton = (ImageButton) findViewById(R.id.StoreButton);
         bankButton = (ImageButton) findViewById(R.id.BankButton);
         rulesButton = (ImageButton) findViewById(R.id.RulesButton);
         optionsButton = (Button) findViewById(R.id.optionsButton);
+        recyclerView = (RecyclerView) findViewById(R.id.TaskRecycler);
 
         taskButton.setOnClickListener(switchPage);
         storeButton.setOnClickListener(switchPage);
@@ -77,6 +137,9 @@ public class TasksActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 currentUser = dataSnapshot.getValue(User.class);
+                if(currentUser != null){
+                    currentUser.setUserID(mUser.getUid());
+                }
             }
 
             @Override
@@ -85,9 +148,7 @@ public class TasksActivity extends AppCompatActivity {
             }
         });
 
-        if(currentUser != null){
-            currentUser.setUserID(mUser.getUid());
-        }
+
     }
 
     View.OnClickListener switchPage = new View.OnClickListener() {
