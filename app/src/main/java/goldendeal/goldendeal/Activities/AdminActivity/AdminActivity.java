f63b8501256cd4e5de9e2049a409081f41dcac78
Tage Veflingstad <Tage.veflingstad@gmail.com>
2019-02-04 14:16:07 +0100

    setting up AdminActivity
    setting up AdminActivity

---
+++ app/src/main/java/goldendeal/goldendeal/Activities/AdminActivity/AdminActivity.java
@@ -0,0 +1,15 @@
+package goldendeal.goldendeal.Activities.AdminActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import goldendeal.goldendeal.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
