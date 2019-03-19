feac7c7d556a4193dd801179ff3cc6076377ab8e
Tage Veflingstad <Tage.veflingstad@gmail.com>
2019-03-19 16:52:21 +0100

    prepearing for work on the store page
    setting up storeitem class for working on  the store page

---
+++ app/src/main/java/goldendeal/goldendeal/Model/StoreItem.java
@@ -0,0 +1,8 @@
+package goldendeal.goldendeal.Model;
+
+public class StoreItem {
+    private String title;
+    private String description;
+    private String imageURL;
+    private VirtualCurrency currency;
+}
